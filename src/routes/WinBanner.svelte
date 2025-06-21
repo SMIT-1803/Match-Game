8db2907d682e80c2c7730130c1421c3bb9ad514a
SMIT-1803 <smitsachin1@gmail.com>
2025-06-21 13:17:25 -0700

    Edited new game button

--- src/routes/WinBanner.svelte
+++ src/routes/WinBanner.svelte
@@ -7,5 +7,5 @@
 
 <div class="mx-auto max-w-sm rounded-2xl bg-white p-8 text-center shadow-xl">
 	<h1 class="text-neutral-dark mb-6 text-2xl font-extrabold sm:text-3xl">Congratulations!</h1>
-	<button on:click={handleClick} class="btn-primary px-6 py-3"> New Game </button>
+	<button on:click={handleClick} class="btn-primary px-6 py-3 bg-black text-white rounded-md"> New Game </button>
 </div>
